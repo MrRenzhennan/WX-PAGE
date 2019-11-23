bb8a8c20a6d9caa59e4e8cdf6950a85aee8308fe
ren <532349787@qq.com>
2019-11-23 22:19:27 +0800

    add

--- 诵读小程序/pages/production/production.js
+++ 诵读小程序/pages/production/production.js
@@ -5,14 +5,41 @@ Page({
    * 页面的初始数据
    */
   data: {
-
+    tabArray: ['小学组', '初中组', '高中组', '大学组', '初中组', '高中组', '大学组'],
+    searchArray: ['综合推荐', '最新参与', '得票最多'],
+    activeIdx: 0,
+    searchIdx: 0,
+  },
+  currentTab: function (e) {
+    let index = e.currentTarget.dataset.index;
+    this.setData({
+      activeIdx: index
+    })
+  },
+  searchTab: function (e) {
+    let index = e.currentTarget.dataset.index;
+    this.setData({
+      searchIdx: index
+    })
+  },
+  //swiper切换
+  currentChange: function (e) {
+    let index = e.detail.current
+    this.setData({
+      searchIdx: index,
+    })
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-
+    var query = wx.createSelectorQuery();
+    query.select('.top').boundingClientRect((rect) => {
+      this.setData({
+        titleHeight: rect.height
+      })
+    }).exec();
   },
 
   /**
